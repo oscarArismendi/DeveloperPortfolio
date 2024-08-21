374d4a475ddb339790f64beac77ba0c2c14890b7
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-08-20 21:41:27 -0500

    feat: add cv download button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,8 @@ import { Footer } from './Footer'
 import profilePicture from '../assets/formarCamisaAzulSonriente.jpeg'; 
 import githubImage from "../assets/github.png";
 import linkedinImage from "../assets/linkedin.png";
+import englishCV from "../assets/CVOscarArismendiEng.pdf"
+import downloadImage from "../assets/download.png"
 export const Home = () =>{
 
   return (<>
@@ -19,7 +21,7 @@ export const Home = () =>{
         Hi! I'm Oscar Arismendi, a Back-end Developer with expertise in Spring Boot, Java, SQL databases and React. I help businesses and teams build robust and innovative software solutions. Shall we talk?
         </p>
         <div className="links-container">
-                <h2 className="links-container-subtitle">Access my social profiles:</h2>
+                <h2 className="links-container-subtitle">Get in Touch & Learn More:</h2>
                 <ContactLink
                   link={"https://www.linkedin.com/in/oscar-arismendi/"}
                   imagePath={linkedinImage}
@@ -32,6 +34,10 @@ export const Home = () =>{
                   text={"Github"}
                   >
                 </ContactLink>
+                <a href={englishCV} className="blue-link" download>
+                <i class="fa-solid fa-download"></i>
+                  <p className="blue-link-text">CV</p>
+                </a>
         </div>
         </section>
         <img id="profile-picture" src={profilePicture} alt="Imagen de Oscar Arismendi"></img>
